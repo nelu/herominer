94fa817c71285bbb6554a9146aba58c307894ea3
nelu <nelu@github.com>
2025-05-23 01:21:54 +0300

    write filter events for short check run task

--- app/game/events.py
+++ app/game/events.py
@@ -1,6 +1,7 @@
 from app.game import play_action, config as game_config
 from app.utils.log import logger
 from app.utils.session import status
+from app.driver import player as driver
 
 log = logger(__name__)
 DATA = status()
@@ -15,9 +16,13 @@ def has_event(ev_names, ev_type="special-events"):
     return False
 
 
-def has_low_interval_special_events():
+def has_short_interval_special_events():
     return has_event(game_config().get('short_interval_special_events'))
 
 
-def check_special_events():
+def check_special_events(short_interval=False):
+    driver.set_run_inputs(
+        {'event-filters.txt': short_interval and " ".join(
+            game_config().get('short_interval_special_events')) or " "
+         })
     return play_action(f"events/check", True)
